1d0d5f48d168fcb92d99c00075aa701b6f3e79b6
Agustin Nieto <agusnieto.gg@gmail.com>
2024-03-18 12:48:09 -0500

    Improve about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Typography, Link } from "@mui/material";
 
 const AboutPage = () => {
   const myEmail = "anietog1@eafit.edu.co";
@@ -7,14 +7,17 @@ const AboutPage = () => {
     <main>
       <Typography>
         If you have a suggestion, please get in touch with us at email:
-        <a href={`mailto:${myEmail}`}>{myEmail}</a>
+        <Link href={`mailto:${myEmail}`}>{myEmail}</Link>
       </Typography>
 
       <Typography>
         In case you want to check our database, here it is:
-        <a href="https://docs.google.com/spreadsheets/d/1l5E_TxjhjFtqWKD29slE9YzBOd7Y7LO3n2jxNfINlB8/edit?usp=sharing">
+        <Link
+          href="https://docs.google.com/spreadsheets/d/1l5E_TxjhjFtqWKD29slE9YzBOd7Y7LO3n2jxNfINlB8/edit?usp=sharing"
+          target="_blank"
+        >
           Database
-        </a>
+        </Link>
       </Typography>
     </main>
   );
