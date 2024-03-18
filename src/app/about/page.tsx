1ce56e0df220a919cbe2fc2d8d5b4a0c55cb97c3
Agustin Nieto <agusnieto.gg@gmail.com>
2024-03-18 11:07:44 -0500

    Format with prettier

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,25 +1,23 @@
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 
 const AboutPage = () => {
-    const myEmail = "anietog1@eafit.edu.co";
+  const myEmail = "anietog1@eafit.edu.co";
 
-    return (
-        <main>
-            <Typography>
-                If you have a suggestion, please get in touch with us at email:
-                <a href={`mailto:${myEmail}`}>
-                    {myEmail}
-                </a>
-            </Typography>
+  return (
+    <main>
+      <Typography>
+        If you have a suggestion, please get in touch with us at email:
+        <a href={`mailto:${myEmail}`}>{myEmail}</a>
+      </Typography>
 
-            <Typography>
-                In case you want to check our database, here it is:
-                <a href="https://docs.google.com/spreadsheets/d/1l5E_TxjhjFtqWKD29slE9YzBOd7Y7LO3n2jxNfINlB8/edit?usp=sharing">
-                    Database
-                </a>
-            </Typography>
-        </main>
-    );
-}
+      <Typography>
+        In case you want to check our database, here it is:
+        <a href="https://docs.google.com/spreadsheets/d/1l5E_TxjhjFtqWKD29slE9YzBOd7Y7LO3n2jxNfINlB8/edit?usp=sharing">
+          Database
+        </a>
+      </Typography>
+    </main>
+  );
+};
 
 export default AboutPage;
