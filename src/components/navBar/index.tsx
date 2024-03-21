d5a90f833717a42575f15ccb99a7e1765b6679eb
Agustin Nieto <agusnieto.gg@gmail.com>
2024-03-21 13:03:24 -0500

    Improve navbar

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,22 +1,17 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 
 const NavBar = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="transparent">
+    <Box>
+      <AppBar position="relative" color="inherit">
         <Toolbar>
-          <Link variant="h6" sx={{ flexGrow: 1 }} href="/">
-            Home
-          </Link>
-          <Link variant="h6" sx={{ flexGrow: 1 }} href="/dashboard">
+          <Link variant="h6" href="/dashboard">
             Dashboard
           </Link>
-          <Link variant="h6" sx={{ flexGrow: 1 }} href="/about">
+          <Link variant="h6" href="/about" marginLeft={1}>
             About
           </Link>
         </Toolbar>
