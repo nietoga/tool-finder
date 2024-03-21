9e886d5ad5345057cf80c8542c026de22e70b470
Agustin Nieto <agusnieto.gg@gmail.com>
2024-03-21 13:28:20 -0500

    Show tool json in tool page

--- src/app/tool/page.tsx
+++ src/app/tool/page.tsx
@@ -1,5 +1,15 @@
-const ToolsPage = () => {
-  return <main>Tools</main>;
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+import { data } from "../dashboard/data";
+
+const ToolPage = () => {
+  const searchParams = useSearchParams();
+  const name = searchParams.get("name");
+
+  const toolData = data.find((tool) => tool.name === name);
+  return <div>{toolData ? JSON.stringify(toolData) : "Not Found"}</div>;
 };
 
-export default ToolsPage;
+export default ToolPage;
