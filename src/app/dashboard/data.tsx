507278d2a4251a96dbc695599c0688fadd560493
Agustin Nieto <agusnieto.gg@gmail.com>
2024-03-21 11:54:14 -0500

    Export filterable columns list

--- src/app/dashboard/data.tsx
+++ src/app/dashboard/data.tsx
@@ -24,8 +24,12 @@ const singleValuedColumnIds = ["name", "description", "main_url"];
 
 const fileData = tsvFile as Object[];
 
-export const columnNames = fileData[0];
+export const columnNames = fileData[0] as { [k: string]: string };
 export const columnIds = Object.keys(columnNames);
+
+export const filterableColumnIds = columnIds.filter(
+  (columnId) => !singleValuedColumnIds.includes(columnId)
+);
 
 export const data: ToolData[] = fileData.slice(1).map((toolData) => {
   const transformedData = {} as ToolData;
